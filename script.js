49d9945bfaa8e1d6f877d0aba0c864a936828b84
msalaroli <salaroli1998@hotmail.com>
2024-11-13 18:18:23 -0300

    importando middleware

--- script.js
+++ script.js
@@ -2,14 +2,29 @@ const express = require('express');
 require('dotenv').config();
 const mysql = require('mysql2');
 const userRoutes = require('./MVC/routes/userRoutes');
+const path = require('path');
+const authMiddleware = require('./MVC/middleware/authMiddleware');
+
 const app = express();
+
+// Configurar o EJS como mecanismo de template
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'MVC', 'view'));
+
+// Middleware para analisar JSON e dados de formulário
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//módulo de rotas
+// Servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Módulo de rotas
 app.use(userRoutes);
 
-const PORT = 3000;
+// Middleware para verificar o token JWT
+app.use(authMiddleware.verifyToken);
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
